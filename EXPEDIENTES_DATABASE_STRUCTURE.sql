d2d951dee6d4f2969f1a8d9bcfabc8c8d399c327
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2023-11-20 18:44:03 -0600

    Actualizacion base de datos

--- EXPEDIENTES_DATABASE_STRUCTURE.sql
+++ EXPEDIENTES_DATABASE_STRUCTURE.sql
@@ -1,5 +1,48 @@
 CREATE DATABASE expedientes_origen_database;
 
+CREATE TABLE IF NOT EXISTS usuarios(
+                                       identificador BIGSERIAL NOT NULL ,
+                                       email VARCHAR NOT NULL UNIQUE ,
+                                       clave_acceso VARCHAR NOT NULL,
+                                       nombre_usuario VARCHAR NOT NULL UNIQUE,
+                                       CONSTRAINT PK_USUARIOS
+                                       PRIMARY KEY(identificador)
+    );
+
+CREATE TABLE IF NOT EXISTS roles(
+                                    identificador BIGSERIAL NOT NULL,
+                                    nivel_acceso VARCHAR NOT NULL UNIQUE,
+                                    CONSTRAINT PK_ROLES
+                                    PRIMARY KEY(identificador)
+    );
+
+CREATE TABLE IF NOT EXISTS roles_usuario(
+                                            usuario_identificador BIGINT NOT NULL,
+                                            rol_identificador BIGINT NOT NULL,
+                                            CONSTRAINT FK_USUARIOS
+                                            FOREIGN KEY(usuario_identificador) REFERENCES usuarios(identificador),
+    CONSTRAINT FK_ROLES
+    FOREIGN KEY(rol_identificador) REFERENCES roles(identificador)
+    );
+
+CREATE TABLE IF NOT EXISTS entidades(
+                                        identificador INTEGER NOT NULL UNIQUE,
+                                        nombre_entidad VARCHAR NOT NULL UNIQUE,
+                                        CONSTRAINT PK_ENTIDADES
+                                        PRIMARY KEY (identificador)
+
+    );
+
+CREATE TABLE IF NOT EXISTS permisos_roles(
+                                             identificador_rol BIGINT NOT NULL,
+                                             identificador_entidad INTEGER NOT NULL,
+                                             accion_permitida SMALLINT NOT NULL,
+                                             CONSTRAINT FK_ROLES
+                                             FOREIGN KEY(identificador_rol) REFERENCES roles(identificador),
+    CONSTRAINT FK_ENTIDADES
+    FOREIGN KEY(identificador_entidad) REFERENCES entidades(identificador)
+    );
+
 CREATE TABLE IF NOT EXISTS secciones(
                                         clave VARCHAR NOT NULL,
                                         nombre VARCHAR NOT NULL,
@@ -69,6 +112,7 @@ CREATE TABLE IF NOT EXISTS expedientes(
     condicion_acceso SMALLINT NOT NULL,
     tradicion_documental SMALLINT NOT NULL,
     tipo_informacion SMALLINT NOT NULL,
+    google_drive_folder_id VARCHAR NOT NULL,
     CONSTRAINT PK_EXPEDIENTES
     PRIMARY KEY (serie_documental, unidad_administrativa_generadora, numero_expediente, fecha_apertura),
     CONSTRAINT FK_SERIE_DOCUMENTAL
@@ -80,11 +124,23 @@ CREATE TABLE IF NOT EXISTS expedientes(
 CREATE TABLE IF NOT EXISTS documentos(
                                          identificador BIGSERIAL NOT NULL,
                                          nombre VARCHAR NOT NULL,
-                                         url VARCHAR NULL,
+                                         urlWebView VARCHAR NULL,
+                                         google_drive_file_id VARCHAR NOT NULL,
                                          fecha_creacion timestamptz NOT NULL DEFAULT now(),
     fecha_edicion timestamptz NOT NULL DEFAULT now(),
     CONSTRAINT PK_DOCUMENTOS PRIMARY KEY (identificador)
     );
+
+/**
+  Codigo Permisos
+
+  1 Ver,
+  2 Crear,
+  3 Editar,
+  4 Marcar como Eliminado,
+  5 Eliminar desde BD
+
+  */
 
 CREATE TABLE IF NOT EXISTS legajos(
                                       serie_documental_expediente SMALLINT NOT NULL,
@@ -96,6 +152,7 @@ CREATE TABLE IF NOT EXISTS legajos(
                                       letra_estante VARCHAR NOT NULL,
                                       numero_pasillo VARCHAR NOT NULL,
                                       letra_bateria VARCHAR NOT NULL,
+                                      google_drive_folder_id VARCHAR NOT NULL,
                                       CONSTRAINT PK_LEGAJOS
                                       PRIMARY KEY (serie_documental_expediente, unidad_administrativa_generadora_expediente, numero_expediente, fecha_apertura_expediente, numero_legajo),
     CONSTRAINT FK_EXPEPEDIENTES
@@ -115,3 +172,35 @@ CREATE TABLE IF NOT EXISTS contenido_legajo(
     REFERENCES legajos(serie_documental_expediente, unidad_administrativa_generadora_expediente, numero_expediente, fecha_apertura_expediente, numero_legajo),
     CONSTRAINT FK_DOCUMENTOS FOREIGN KEY (identificador_documento) REFERENCES documentos(identificador)
     );
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON secciones TO spring_app_usr;
+GRANT SELECT, INSERT, UPDATE, DELETE ON tecnicas_seleccion TO spring_app_usr;
+GRANT SELECT, INSERT, UPDATE, DELETE ON series_documentales TO spring_app_usr;
+GRANT USAGE ON series_documentales_identificador_seq to spring_app_usr;
+GRANT SELECT, INSERT, UPDATE, DELETE ON unidades_administrativas TO spring_app_usr;
+GRANT SELECT, INSERT, UPDATE, DELETE ON expedientes TO spring_app_usr;
+GRANT USAGE ON documentos_identificador_seq to spring_app_usr;
+GRANT SELECT, INSERT, UPDATE, DELETE ON documentos TO spring_app_usr;
+GRANT SELECT, INSERT, UPDATE, DELETE ON legajos TO spring_app_usr;
+GRANT SELECT, INSERT, UPDATE, DELETE ON contenido_legajo to spring_app_usr;
+
+INSERT INTO secciones(clave, nombre, descripcion) VALUES
+                                                      ('1C', 'LEGISLACIÓN', ''), ('2C', 'ASUNTOS JURIDICOS', ''), ('1S', 'GOBIERNO', '');
+INSERT INTO tecnicas_seleccion(identificador, tecnica_seleccion) VALUES
+                                                                     (1, 'CONSERVACION'), (2, 'ELIMINACION'), (3, 'MUESTREO');
+
+INSERT INTO series_documentales(
+    serie_documental_padre, clave, seccion, nombre, valor_documental_administrativo,
+    valor_documental_legal, valor_documental_contable, periodos_conservacion_tramite,
+    periodos_conservacion_concentracion, tecnica_seleccion, observaciones) VALUES
+                                                                               (null, '10', '1C', 'INSTRUMENTOS JURIDICOS', '1', '1', '0', 2, 5, 1, ''),
+                                                                               (NULL ,'3', '2C', 'REGISTRO Y CERTIFICACION DE FIRMAS', '1', '1', '0', 2, 5, 2, ''),
+                                                                               (NULL, '4', '2C', 'Registro y certificación de firmas acreditadas ante la dependencia ', '1', '1', '0', 2, 5, 1, ''),
+                                                                               (NULL, '1', '1S', 'Órganos colegiados', '1', '0', '0', 2, 5, 3, '');
+
+INSERT INTO series_documentales (serie_documental_padre, clave, seccion, nombre, valor_documental_administrativo, valor_documental_legal, valor_documental_contable, periodos_conservacion_tramite, periodos_conservacion_concentracion, tecnica_seleccion, observaciones) VALUES
+    (4, '1', NULL, 'Comité Técnico', '1', '0', '0', 2, 5, 2, '');
+
+INSERT INTO unidades_administrativas(clave, nombre, unidad_principal, piso, extension_telefonica) VALUES ('DAAP', 'NO SE', NULL, '1', '12345');
+
+SELECT * FROM contenido_legajo;
