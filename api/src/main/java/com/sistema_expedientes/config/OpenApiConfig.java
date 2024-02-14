fe9fb3fd240d73149abddc6585322f02060d7341
josafat <josa221001@gmail.com>
2024-02-13 19:08:03 -0600

    add: creando encabezado de la documentacion con swagger

--- api/src/main/java/com/sistema_expedientes/config/OpenApiConfig.java
+++ api/src/main/java/com/sistema_expedientes/config/OpenApiConfig.java
@@ -1,2 +1,16 @@
-package com.sistema_expedientes.config;public class OpenApiConfig {
+package com.sistema_expedientes.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Gestion de Expedientes API - INFOTEC",
+                version = "1.0.0",
+                description = "Api para la gestión de expedientes de manera semiautomatizada"
+        )
+)
+public class OpenApiConfig {
 }
