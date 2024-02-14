c1c4b6ea01ed8b9fd06bdd04de8e555329b1b16d
josafat <josa221001@gmail.com>
2024-02-13 19:12:33 -0600

    docs: documentando endpoint para swagger

--- api/src/main/java/com/sistema_expedientes/controllers/AdminController.java
+++ api/src/main/java/com/sistema_expedientes/controllers/AdminController.java
@@ -3,6 +3,8 @@ package com.sistema_expedientes.controllers;
 import com.sistema_expedientes.auth.Role;
 import com.sistema_expedientes.auth.User;
 import com.sistema_expedientes.services.auth.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/admin")
+@Tag(name = "Admin Controller")
 public class AdminController {
 
     private final UserService userService;
@@ -18,11 +21,13 @@ public class AdminController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Regresa el nivel de acceso conforme al rol admin")
     @GetMapping
     public String getLevel(){
         return "Admin level";
     }
 
+    @Operation(summary = "Modificar el rol de los usuarios")
     @PutMapping("/{username}/role")
     public ResponseEntity<String> changueRole(@PathVariable String username, @RequestBody Role role){
         Optional<User> existUser = userService.findUserByUsername(username);
